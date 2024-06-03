8170032809265c8dbc0c1c8a3789b2c90e881cde
khangancam <55498491+khangancam@users.noreply.github.com>
2024-06-03 15:33:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
-﻿require('rootpath')();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const errorHandler = require('_middleware/error-handler');
+var express = require('express');
+var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
 
-// api routes
-// app.use('/users', require('./users/users.controller'));
-
-// global error handler
-//app.use(errorHandler);
-
-// start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
-app.listen(port, () => console.log('Server listening on port ' + port));
+var server = app.listen(5000, function () {
+   console.log("Express App running at http://127.0.0.1:5000/");
+})
